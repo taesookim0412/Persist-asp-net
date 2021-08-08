3eed84e78f70ef679a736b06a68d0c42260f56bf
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-08-08 00:27:50 -0700

    `

---
+++ Controllers/GatewayController.cs
@@ -0,0 +1,7 @@
+﻿namespace Persist_asp_net.Controllers
+{
+    public class GatewayController
+    {
+        
+    }
+}
---
+++ Models/Server.cs
@@ -0,0 +1,8 @@
+﻿namespace Persist_asp_net.Models
+{
+    public class Servers
+    {
+        public string ip { get; set; };
+        public string port { get; set; };
+    }
+}
